fb39db0eddcd2eadd63fae1a7e089e368e7ca018
bazhil <i-bazh@mail.ru>
2022-11-25 03:44:11 +0300

    Return files and code from old version to work with Jenkins.

--- scripts/nginx.sh
+++ scripts/nginx.sh
@@ -9,6 +9,7 @@ chmod 710 /var/lib/jenkins/workspace/movies-website
 sudo ln -s /etc/nginx/sites-available/movies-website /etc/nginx/sites-enabled
 sudo nginx -t
 
+systemctl daemon-reload
 sudo systemctl start nginx
 sudo systemctl enable nginx
 
