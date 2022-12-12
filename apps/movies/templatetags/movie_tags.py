b1903ff70251ee0b599c2f6f438cf5892f1b56cc
bazhil <i-bazh@mail.ru>
2022-12-13 02:00:37 +0300

    Add tags and templatetags. Need fix.

--- apps/movies/templatetags/movie_tags.py
+++ apps/movies/templatetags/movie_tags.py
@@ -8,7 +8,9 @@ def get_categories():
     return Category.objects.all()
 
 # TODO: не обнаруживается данный путь - на интерфейсе ошибка - полечить!
-@register.inclusion_tag('apps/movies/templates/tags/last_movies.html')
-def get_last_movies():
-    movies = Movie.objects.order_by('id')[:5]
+#  - внутри метода фильмы получаются, но видимо, не попадают на форму
+@register.inclusion_tag('movies-website/apps/movies/tags/last_movies.html')
+def get_last_movies(count=5):
+    movies = Movie.objects.order_by('id')[:count]
+
     return {'last_movies': movies}
