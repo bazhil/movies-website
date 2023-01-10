46d9fbecf19a54f7c9463fa225d19daec090c38f
bazhil <i-bazh@mail.ru>
2023-01-10 23:19:50 +0300

    Add filter. Need fix error with empty form.

--- apps/movies/views.py
+++ apps/movies/views.py
@@ -2,17 +2,27 @@ from django.shortcuts import render, redirect
 from django.views.generic.base import View
 from django.views.generic import ListView, DetailView
 
-from .models import Movie, Category, Actor
+from .models import Movie, Category, Actor, Genre
 from .forms import ReviewForm
 
-class MovieView(ListView):
+
+class GenreYear:
+    """Жанры и года выхода фильмов"""
+    def get_genres(self):
+        return Genre.objects.all()
+
+    def get_years(self):
+        return Movie.objects.filter(draft=False).values('year')
+
+
+class MovieView(GenreYear, ListView):
     """List of movies"""
     model = Movie
     queryset = Movie.objects.filter(draft=False)
     template_name = 'movie_list.html'
 
 
-class MovieDetailView(DetailView):
+class MovieDetailView(GenreYear, DetailView):
     """Описание фильма"""
 
     model = Movie
@@ -33,8 +43,15 @@ class AddReview(View):
             form.save()
         return redirect(movie.get_absolute_url())
 
-class ActorView(DetailView):
+class ActorView(GenreYear, DetailView):
     """Вывод информации об актере"""
     model = Actor
     template_name = 'actor.html'
     slug_field = 'name'
+
+
+class FilterMoviesView(GenreYear, ListView):
+    """Фильтр фильмов"""
+    def get_queryset(self):
+        queryset = Movie.objects.filter(year__in=self.request.GET.get('year'))
+        return queryset
