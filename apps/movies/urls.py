a42ad6590477082e0eb11b295a734e13ca86fe33
bazhil <i-bazh@mail.ru>
2023-01-18 00:50:59 +0300

    Add rating in movie-detail. Need fix.

--- apps/movies/urls.py
+++ apps/movies/urls.py
@@ -5,6 +5,7 @@ from . import views
 urlpatterns = [
     path('', views.MovieView.as_view()),
     path('filter/', views.FilterMoviesView.as_view(), name='filter'),
+    path("add-rating/", views.AddStarRating.as_view(), name='add_rating'),
     path('json-filter/', views.JsonFilterMoviesView.as_view(), name='json-filter'),
     path('<slug:slug>/', views.MovieDetailView.as_view(), name='movie_detail'),
     path('review/<int:pk>/', views.AddReview.as_view(), name='add_review'),
