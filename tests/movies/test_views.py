8106bcb1aa7ccf38007b5ce92f5222a7326f72fe
bazhil <i-bazh@mail.ru>
2023-02-02 03:15:47 +0300

    Start work on tests.

--- tests/movies/test_views.py
+++ tests/movies/test_views.py
@@ -1,2 +1,12 @@
 import pytest
-from django.test import TestCase
+from django.test import TestCase, Client
+from django.urls import reverse
+from apps.movies.models import Movie, MovieShots, Actor, Category
+
+class TestViews(TestCase):
+    def test_movies_list_GET(self):
+        client = Client()
+        response = client.get(reverse('movies'))
+
+        self.assertEquals(response.status_code, 200)
+        self.assertTemplateUsed(response, 'apps/movies/templates/movies/movie_list.html')
